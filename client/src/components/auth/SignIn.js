beb6755d70a300b5159b91b89c05b3f88ce6964f
Alan Wang <alan098555@gmail.com>
2019-04-16 15:01:04 +1200

    Error Message Display

--- client/src/components/auth/SignIn.js
+++ client/src/components/auth/SignIn.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import classnames from "classnames";
 
 import "./SignIn.css";
 
@@ -10,7 +11,7 @@ class SignIn extends Component {
     this.state = {
       email: "",
       password: "",
-      error: {}
+      errors: {}
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
@@ -30,10 +31,11 @@ class SignIn extends Component {
     axios
       .post("/api/users/signIn", user)
       .then(res => console.log(res.data))
-      .catch(err => console.log(err));
+      .catch(err => this.setState({ errors: err.response.data }));
   }
 
   render() {
+    const { errors } = this.state;
     return (
       <section id="signIn">
         <div className="signInHeader">
@@ -41,25 +43,36 @@ class SignIn extends Component {
           <p>Sign In To Keep Fit</p>
         </div>
 
-        <form onSubmit={this.onSubmit}>
+        <form noValidate onSubmit={this.onSubmit}>
           <div className="inputGroup">
             <input
               type="email"
-              className=""
+              className={classnames("", {
+                "is-invalid": errors.email
+              })}
               placeholder="Email Address"
               name="email"
               value={this.state.email}
               onChange={this.onChange}
             />
-
+            <div className="errorText">
+              {errors.email && <p className="invalidMsg">{errors.email}</p>}
+            </div>
             <input
               type="password"
-              className=""
+              className={classnames("", {
+                "is-invalid": errors.password
+              })}
               placeholder="Password"
               name="password"
               value={this.state.password}
               onChange={this.onChange}
             />
+            <div className="errorText">
+              {errors.password && (
+                <p className="invalidMsg">{errors.password}</p>
+              )}
+            </div>
             <input type="submit" value="Sign In" className="signInBtn" />
           </div>
           <p className="or">or</p>
--- client/src/components/auth/SignUp.js
+++ client/src/components/auth/SignUp.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import classnames from "classnames";
 import axios from "axios";
 
 import "./SignUp.css";
@@ -12,7 +13,7 @@ class SignUp extends Component {
       email: "",
       password: "",
       password2: "",
-      error: {}
+      errors: {}
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
@@ -35,10 +36,12 @@ class SignUp extends Component {
     axios
       .post("/api/users/signUp", newUser)
       .then(res => console.log(res.data))
-      .catch(err => console.log(err));
+      .catch(err => this.setState({ errors: err.response.data }));
   }
 
   render() {
+    const { errors } = this.state;
+
     return (
       <section id="signUp">
         <div className="signUpHeader">
@@ -46,46 +49,67 @@ class SignUp extends Component {
           <p>Sign Up To Keep Fit</p>
         </div>
 
-        <form onSubmit={this.onSubmit}>
+        <form noValidate onSubmit={this.onSubmit}>
           <div className="inputGroup">
             <input
               type="text"
-              className=""
+              className={classnames("", {
+                "is-invalid": errors.name
+              })}
               placeholder="Name"
               name="name"
               value={this.state.name}
               onChange={this.onChange}
             />
-
+            <div className="errorText">
+              {errors.name && <p className="invalidMsg">{errors.name}</p>}
+            </div>
             <input
               type="email"
-              className=""
+              className={classnames("", {
+                "is-invalid": errors.email
+              })}
               placeholder="Email Address"
               name="email"
               value={this.state.email}
               onChange={this.onChange}
             />
-
+            <div className="errorText">
+              {errors.email && <p className="invalidMsg">{errors.email}</p>}
+            </div>
             <input
               type="password"
-              className=""
+              className={classnames("", {
+                "is-invalid": errors.password
+              })}
               placeholder="Password"
               name="password"
               value={this.state.password}
               onChange={this.onChange}
             />
-
+            <div className="errorText">
+              {errors.password && (
+                <p className="invalidMsg">{errors.password}</p>
+              )}
+            </div>
             <input
               type="password"
-              className=""
+              className={classnames("", {
+                "is-invalid": errors.password2
+              })}
               placeholder="Confirm Password"
               name="password2"
               value={this.state.password2}
               onChange={this.onChange}
             />
-
+            <div className="errorText">
+              {errors.password2 && (
+                <p className="invalidMsg">{errors.password2}</p>
+              )}
+            </div>
             <input type="submit" value="Sign Up" className="signUpBtn" />
           </div>
+
           <p className="or">or</p>
           <Link to="/signIn">Sign In</Link>
         </form>
