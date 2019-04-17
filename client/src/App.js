31333a73dc94594d1bb1ce25b3a6110b0bdf7248
Alan Wang <alan098555@gmail.com>
2019-04-17 19:15:32 +1200

    Redux Store & rootReducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
 import MobileNavbar from "./components/layout/MobileNavbar";
 import Landing from "./components/layout/Landing";
 import Me from "./components/layout/Me";
@@ -13,16 +16,22 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/signIn" component={SignIn} />
-          <Route exact path="/signUp" component={SignUp} />
-          <Route exact path="/users/me" component={Me} />
-          <Route exact path="/profile/me/editProfile" component={EditProfile} />
-          <MobileNavbar />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/signIn" component={SignIn} />
+            <Route exact path="/signUp" component={SignUp} />
+            <Route exact path="/users/me" component={Me} />
+            <Route
+              exact
+              path="/profile/me/editProfile"
+              component={EditProfile}
+            />
+            <MobileNavbar />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
