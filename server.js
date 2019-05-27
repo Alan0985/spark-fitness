1e41f0ee295ecf043a5c748fd38120deba4e7224
Alan Wang <alan098555@gmail.com>
2019-05-27 16:05:44 +1200

    API

--- server.js
+++ server.js
@@ -10,18 +10,6 @@ const explores = require("./routes/api/explores");
 
 const app = express();
 
-// Serve any static files built by React
-// app.use(express.static(path.join(__dirname, "client/build")));
-
-//Check Production Mode
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-app.get("/routes/api/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
-
 //Image Upload To Cloudinary
 require("dotenv").config();
 const cloudinary = require("cloudinary");
@@ -72,6 +60,18 @@ require("./key/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/posts", posts);
 app.use("/api/explores", explores);
+
+// Serve any static files built by React
+app.use(express.static(path.join(__dirname, "client/build")));
+
+//Check Production Mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("/routes/api/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server Running On Port ${port}`));
