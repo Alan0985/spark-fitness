d3ce5caacfc68463880c8c26c871e1a523bce782
Alan Wang <alan098555@gmail.com>
2019-05-27 15:09:10 +1200

    API

--- server.js
+++ server.js
@@ -21,6 +21,15 @@ app.get("/routes/api/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 //Image Upload To Cloudinary
 require("dotenv").config();
 const cloudinary = require("cloudinary");
