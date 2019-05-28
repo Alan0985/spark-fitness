3f611e8aed3db82c426c76588e5cea2a5cef1119
Alan Wang <alan098555@gmail.com>
2019-05-29 10:49:44 +1200

    Deploy

--- server.js
+++ server.js
@@ -71,19 +71,19 @@ app.use("/api/posts", posts);
 app.use("/api/explores", explores);
 
 // Serve any static files built by React
-// app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 //Check Production Mode
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
   app.get("/routes/api/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-// app.get("/routes/api/*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server Running On Port ${port}`));
