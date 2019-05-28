a30cb63a5a6ac574fe883baec15ecb1a5d37d0bb
Alan Wang <alan098555@gmail.com>
2019-05-29 10:53:31 +1200

    Deploy

--- server.js
+++ server.js
@@ -81,9 +81,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client/build", "index.html"));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server Running On Port ${port}`));
