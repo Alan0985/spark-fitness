ef988801e1537fdfa03ee0552a7f7040703c2f9b
Alan Wang <alan098555@gmail.com>
2019-05-29 21:02:49 +1200

    Cloudinary API

--- client/src/components/layout/me/EditProfile.js
+++ client/src/components/layout/me/EditProfile.js
@@ -61,7 +61,8 @@ class EditProfile extends Component {
     // })
     fetch("https://api.cloudinary.com/v1_1/dgmvfyzua/image/upload", {
       method: "POST",
-      file: formData,
+      body: formData,
+      mode: "cors",
       upload_preset: "xeest4yh"
     })
       .then(res => {
