460785cafec83ea3603c361e56714f5c97e43238
Alan Wang <alan098555@gmail.com>
2023-05-09 19:51:14 +1200

    Client Script --openssl-legacy-provider

--- key/keys.js
+++ key/keys.js
@@ -1,4 +1,7 @@
 module.exports = {
-  mongoURI: process.env.mongoURI,
-  secretOrKey: process.env.secretOrKey
+  // mongoURI: process.env.mongoURI,
+  // secretOrKey: process.env.secretOrKey,
+  mongoURI:
+    "mongodb+srv://alan123:6BEfPUndZyDPSFv6@sf.ofvaf.mongodb.net/?retryWrites=true&w=majority",
+  secretOrKey: "secretOrKey",
 };
